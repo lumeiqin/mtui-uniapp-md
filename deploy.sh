4284bfbc52f9c0c770a076336cbbf9c74d6a21d4
lumeiqin <meiqin.lu@gisocn.com>
2024-04-15 17:02:10 +0800

    更新

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:lacey-lmq/mtui-uniapp.git master:gh-pages
+ git push -f git@github.com:lumeiqin/mtui-uniapp.git main:gh-pages
 
 
 cd -
